5d2ddb0f2e963cb86055a1c4d2cbcc3b4192d04b
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2022-09-25 22:27:59 -0300

    Iniciando Repositorio Portfolio FullStack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { FormacionPortfolioComponent } from './componentes/formacion-portfolio/formacion-portfolio.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    EncabezadoComponent,
+    SobreMiComponent,
+    FormacionPortfolioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
