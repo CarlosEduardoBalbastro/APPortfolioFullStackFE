7d56c496b82dba570da4beb13b2279a68b235ab2
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2023-04-29 16:49:55 -0300

    corrigiendo variable de url en enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL: 'https://apportfoliofullstackbe-production.up.railway.app/'
+  url: 'https://apportfoliofullstackbe-production.up.railway.app/'
 };
